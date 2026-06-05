c92ca4b75c85d9646fc73c4e532b78faf3cb42d0
Aya Ibrahem <ayaibrahema123321@gmail.com>
2026-06-05 12:54:09 +0300

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,48 +1,23 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { Header } from '../components/site/Header'
-import { Footer } from '../components/site/Footer'
-import { useState, useEffect } from 'react'
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { Toaster } from "sonner";
+import { Header } from "~/components/site/Header";
+import { Footer } from "~/components/site/Footer";
 
 export const Route = createRootRoute({
-  component: RootLayout,
-})
+  component: RootComponent,
+});
 
-function RootLayout() {
-  const [isDark, setIsDark] = useState(false)
-
-  useEffect(() => {
-    const savedTheme = localStorage.getItem('theme')
-    const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-    
-    if (savedTheme === 'dark' || (!savedTheme && systemPrefersDark)) {
-      setIsDark(true)
-      document.documentElement.classList.add('dark')
-    } else {
-      setIsDark(false)
-      document.documentElement.classList.remove('dark')
-    }
-  }, [])
-
-  const toggleTheme = () => {
-    const newIsDark = !isDark
-    setIsDark(newIsDark)
-    
-    if (newIsDark) {
-      document.documentElement.classList.add('dark')
-      localStorage.setItem('theme', 'dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-      localStorage.setItem('theme', 'light')
-    }
-  }
-
+function RootComponent() {
   return (
-    <div className="min-h-screen flex flex-col font-almarai" dir="rtl">
-      <Header isDark={isDark} toggleTheme={toggleTheme} />
-      <main className="flex-1">
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
-  )
+    <>
+      <div className="relative min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+      <Toaster position="bottom-right" richColors />
+    </>
+  );
 }
