0f268591a114976dbc917aa64009cf84770c34db
Aya Ibrahem <ayaibrahema123321@gmail.com>
2026-06-05 12:54:40 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ 
-      target: 'react',
-      autoCodeSplitting: true,
+    TanStackRouterVite({
+      routesDirectory: "./src/routes",
+      generatedRouteTree: "./src/routeTree.gen.ts",
     }),
     react(),
     tailwindcss(),
+    tsconfigPaths(),
   ],
   resolve: {
     alias: {
-      '~': path.resolve(__dirname, './src'),
+      "~": "/src",
     },
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
   },
   server: {
     port: 3000,
   },
-})
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
+});
